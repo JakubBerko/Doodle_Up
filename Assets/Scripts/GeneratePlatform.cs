11f78e12cba4a31e9ebbcfeb53eee54f4e352af7
JakubBerko <93920505+JakubBerko@users.noreply.github.com>
2023-01-12 09:10:43 +0100

    MapGeneration
    Added dynamic MapGeneration

---
+++ Assets/Scripts/GeneratePlatform.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratePlatform : MonoBehaviour
+{
+    public Transform platform;
+    public GameObject player;
+
+    private Transform playerTrans;
+    private float platformsSpawnLimit = 0.0f;
+    private ArrayList platforms;
+    private float spawnMorePlatformsIn = 0.0f;
+
+
+    void Awake()
+    {
+        playerTrans = player.transform;
+        platforms = new ArrayList();
+
+        GeneratePlatforms(30.0f);
+    }
+
+    void Update()
+    {
+        //Do we need to spawn new platforms yet? (we do this every X meters we climb)
+        float playerY = playerTrans.position.y;
+        if (playerY > spawnMorePlatformsIn)
+        {
+            PlatformManager(); //Spawn new platforms
+        }
+    }
+
+    void PlatformManager()
+    {
+        //update next platform check
+        spawnMorePlatformsIn = playerTrans.position.y + 10;
+
+        //Spawn new platforms, 30 units in advance
+        GeneratePlatforms(spawnMorePlatformsIn + 35);
+    }
+    void GeneratePlatforms(float limit)
+    {
+        float spawnPosY = platformsSpawnLimit;
+        while (spawnPosY <= limit)
+        {
+            float spawnPosX = Random.Range(-2.5f, 2.5f);
+            Vector3 spawnPos = new Vector3(spawnPosX, spawnPosY, 12.0f);
+
+            Transform plat = (Transform)Instantiate(platform, spawnPos, Quaternion.identity);
+            platforms.Add(plat);
+
+            spawnPosY += Random.Range(.5f, 1f);
+        }
+        platformsSpawnLimit = limit;
+    }
+}
+
+/*
+//Older code for spawning platforms:
+    public GameObject platformPrefab;
+
+    public int platformCount = 300;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 spawnPos = new Vector3();
+
+        for (int i = 0; i < platformCount; i++)
+        {
+            spawnPos.y += Random.Range(.5f, 1f);
+            spawnPos.x = Random.Range(-2.5f, 2.5f);
+            Instantiate(platformPrefab, spawnPos, Quaternion.identity); 
+        }
+    }*/
