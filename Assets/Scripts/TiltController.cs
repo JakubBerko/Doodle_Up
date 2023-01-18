40c0443a260cf024a772a186904a567be74bd7d0
JakubBerko <93920505+JakubBerko@users.noreply.github.com>
2023-01-18 21:17:37 +0100

    Added ScreenWrap

--- Assets/Scripts/TiltController.cs
+++ Assets/Scripts/TiltController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class TiltController : MonoBehaviour
 {
+    //screenWrap
+    private float halfHeight; 
+    private float halfWidth; 
+
     Rigidbody2D rb;
     float dx; //smìr a rychlost pohybu na x
     float moveSpeed = 20f; //rychlost pohybu
@@ -12,10 +16,26 @@ public class TiltController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>(); //získá RB od Doodlera
     }
+    void Start()
+    {
+        halfHeight = Camera.main.orthographicSize;
+        halfWidth = halfHeight * Camera.main.aspect;
+    }
     void Update()
     {
         //pohyb telefonu * moveSpeed = rychlost  pohybu na daný smìr
         dx = Input.acceleration.x * moveSpeed;
+
+        //screenWrap 
+        if (transform.position.x > halfWidth) //pokud je pozice hráèe na x vìtší než pozice kamery napravo
+        {
+            transform.position = new Vector3(-halfWidth, transform.position.y, transform.position.z); //objeví hráèe na opaèné stranì (-halfWidth) a ponechá výšku a z
+        }
+        else if (transform.position.x < -halfWidth) //pokud je pozice hráèe na x vìtší než pozice kamery nalevo
+        {
+            transform.position = new Vector3(halfWidth, transform.position.y, transform.position.z); //objeví hráèe na opaèné stranì (halfWidth) a ponechá výšku a z
+        }
+
     }
     void FixedUpdate()
     {
